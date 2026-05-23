c3e5fd9b799f7dd72bafdc350efb0b886d3ec046
VA80 <87236325+VihaanReddyM@users.noreply.github.com>
2026-05-23 07:48:01 +0530

    Add PostgreSQL support and connection pooling
    Add HikariCP and PostgreSQL dependencies and a DatabaseManager
    that manages a HikariDataSource and exposes getConnection().
    Load database settings from a new config.yml. Connect on plugin
    enable and disconnect on disable, and expose getDatabaseManager().

--- src/main/java/dev/bwd/seasonpoints/SeasonPointsPlugin.java
+++ src/main/java/dev/bwd/seasonpoints/SeasonPointsPlugin.java
@@ -1,20 +1,33 @@
 package dev.bwd.seasonpoints;
 
+import dev.bwd.seasonpoints.database.connection.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SeasonPointsPlugin extends JavaPlugin {
 
+    private DatabaseManager databaseManager;
+
     @Override
     public void onEnable() {
-
         saveDefaultConfig();
+
+        this.databaseManager = new DatabaseManager(this);
+
+        databaseManager.connect();
 
         getLogger().info("BWD-SeasonPoints enabled!");
     }
 
     @Override
     public void onDisable() {
+        if (databaseManager != null) {
+            databaseManager.disconnect();
+        }
 
         getLogger().info("BWD-SeasonPoints disabled!");
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
     }
 }
---
+++ src/main/java/dev/bwd/seasonpoints/database/connection/DatabaseManager.java
@@ -0,0 +1,60 @@
+package dev.bwd.seasonpoints.database.connection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import dev.bwd.seasonpoints.SeasonPointsPlugin;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    private final SeasonPointsPlugin plugin;
+    private HikariDataSource dataSource;
+
+    public DatabaseManager(SeasonPointsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void connect() {
+
+        String host = plugin.getConfig().getString("database.host");
+        int port = plugin.getConfig().getInt("database.port");
+        String database = plugin.getConfig().getString("database.name");
+        String username = plugin.getConfig().getString("database.username");
+        String password = plugin.getConfig().getString("database.password");
+
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(
+                "jdbc:postgresql://" +
+                host + ":" +
+                port + "/" +
+                database
+        );
+
+        config.setUsername(username);
+        config.setPassword(password);
+
+        config.setMaximumPoolSize(10);
+
+        config.setPoolName("BWD-SeasonPoints");
+
+        this.dataSource = new HikariDataSource(config);
+
+        plugin.getLogger().info("Connected to PostgreSQL!");
+    }
+
+    public void disconnect() {
+
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+
+        plugin.getLogger().info("Disconnected from PostgreSQL!");
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
